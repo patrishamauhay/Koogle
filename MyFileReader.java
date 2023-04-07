c662167a4b186c30f64f128d3f7428c5afa9a589
Patrisha Mauhay <124062924+patrishamauhay@users.noreply.github.com>
2023-04-07 16:24:37 +0100

    Update MyFileReader.java

--- MyFileReader.java
+++ MyFileReader.java
@@ -14,7 +14,29 @@ public class MyFileReader {
     	// Creates object from the file path
         File file = new File(filePath);
         
-        // Object to reMy
+        // Object to read file
+        Scanner reader;
+        
+        // String for file contents
+        String fileContents = "";
+        
+		try {
+			reader = new Scanner(file);
+			
+			// Loops through each line of file
+			while (reader.hasNextLine()) {
+	            fileContents += reader.nextLine() + "\n";
+	        }
+
+			// Close scanner object
+	        reader.close();
+			
+	    // If file is not found, shows error message
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+			System.out.println("Something wrong has happened");
+		}
+       
 		// Returns file contents string
         return fileContents;
     }
