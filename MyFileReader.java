980bb05c704efbfa679f586a98d37e0ce3b6e4db
Patrisha Mauhay <124062924+patrishamauhay@users.noreply.github.com>
2023-04-07 16:20:18 +0100

    MyFileReader class created

---
+++ MyFileReader.java
@@ -0,0 +1,17 @@
+
+import java.io.*;
+import java.util.*;
+
+public class MyFileReader {
+
+    public static String readFile(String filePath) {
+    	
+    	// Creates object from the file path
+        File file = new File(filePath);
+        
+        // Object to reMy
+		// Returns file contents string
+        return fileContents;
+    }
+}
+
