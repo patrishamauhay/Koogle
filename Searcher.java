7fbc101378859465a82822c32fcc9fee7f06b0c3
Patrisha Mauhay <124062924+patrishamauhay@users.noreply.github.com>
2023-04-07 18:53:22 +0100

    Searcher.java completed

--- Searcher.java
+++ Searcher.java
@@ -4,11 +4,9 @@
  */
 
 import java.util.*;
-import java.util.regex.*;
 
 public class Searcher {
     
-	//  
     public static ArrayList<SearchResult> search(String[] filePaths, String term){
     	
     	// Array to store search results
@@ -22,39 +20,47 @@ public class Searcher {
             
             // Calculates the number of appeared terms
             int count = calculate(fileContent, term);
-           
+            
+            // Adds object to list if term appears more than once
             if (count > 0) {
                 searchResults.add(new SearchResult(filePath, count));
             }
         }
         
-        searchResults.sort((a, b) -> b.getScore() - a.getScore());
+        // Sorts results in decreasing order by score of each result
+        Collections.sort(searchResults, Comparator.comparing(SearchResult::getScore).reversed());
         return searchResults;
     }
     
-    
+    // Method that calculates score
     private static int calculate(String fileContent, String term) {
+    	
         int count = 0;
         
+        //Splits searched terms into separate terms
         String[] queryWords = term.split("\\s+");
         
+        // Goes over each search term
         for (String queryWord : queryWords) {
+        	
+        	// If query word ends with "*", remove it and match beginning of word
             if (queryWord.endsWith("*")) {
-            	
-                String regex = queryWord.replace("*", ".*");
-                Pattern pattern = Pattern.compile(regex);
-                Matcher matcher = pattern.matcher(fileContent);
                 
-                while (matcher.find()) {
-                	count++;
+                String wordPrefix = queryWord.substring(0, queryWord.length() - 1);
+                
+                if (fileContent.indexOf(wordPrefix) == 0) {
+                    count++;
                 }
             } else {
+            	
+                // Check if the word is present in the file content
                 if (fileContent.contains(queryWord)) {
-                	count++;
+                    count++;
                 }
             }
         }
+        
         return count;
     }
-}
 
+   }
