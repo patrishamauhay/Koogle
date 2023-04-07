f733000dc3bc000b91352c663bd723f3565182b8
Patrisha Mauhay <124062924+patrishamauhay@users.noreply.github.com>
2023-04-07 16:32:32 +0100

    Update Searcher.java

--- Searcher.java
+++ Searcher.java
@@ -1,1 +1,55 @@
+import java.util.*;
+import java.util.regex.*;
+
+public class Searcher {
+    
+	//  
+    public static ArrayList<SearchResult> search(String[] filePaths, String term){
+    	
+    	// Array to store search results
+        ArrayList<SearchResult> searchResults = new ArrayList<SearchResult>();
+        
+        // Loops over each file path
+        for (String filePath : filePaths) {
+        	
+        	// Reads contents of file
+            String fileContent = MyFileReader.readFile(filePath);
+            
+            // Calculates the number of appeared terms
+            int count = calculate(fileContent, term);
+           
+            if (count > 0) {
+                searchResults.add(new SearchResult(filePath, count));
+            }
+        }
+        
+        searchResults.sort((a, b) -> b.getScore() - a.getScore());
+        return searchResults;
+    }
+    
+    
+    private static int calculate(String fileContent, String term) {
+        int count = 0;
+        
+        String[] queryWords = term.split("\\s+");
+        
+        for (String queryWord : queryWords) {
+            if (queryWord.endsWith("*")) {
+            	
+                String regex = queryWord.replace("*", ".*");
+                Pattern pattern = Pattern.compile(regex);
+                Matcher matcher = pattern.matcher(fileContent);
+                
+                while (matcher.find()) {
+                	count++;
+                }
+            } else {
+                if (fileContent.contains(queryWord)) {
+                	count++;
+                }
+            }
+        }
+        return count;
+    }
+}
 
